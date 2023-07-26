7b430d2384202b8f70e452637ca4265736ad8d78
chenyuyou <chenyuyou@126.com>
2023-07-26 10:26:13 +0800

    add new model

---
+++ pd_punish/batch_run.py
@@ -0,0 +1,122 @@
+# -*- coding: UTF-8 -*-
+import itertools, time
+from mesa import Model
+from mesa.batchrunner import batch_run
+from mesa.datacollection import DataCollector
+from mesa.time import BaseScheduler, RandomActivation, SimultaneousActivation
+from pd_punish.agent import PDAgent
+import numpy as np
+import pandas as pd
+from multiprocessing import cpu_count
+
+def track_run(model):
+    return model.uid
+
+def cooperate(model):
+    return len([a for a in model.schedule.agents if a.move == 0])/model.num_agents
+
+def defect(model):
+    return len([a for a in model.schedule.agents if a.move == 1])/model.num_agents
+
+def punish(model):
+    return len([a for a in model.schedule.agents if a.move == 2])/model.num_agents
+
+class PdNetworkModel(Model):
+
+    id_gen = itertools.count(1)
+    # 博弈顺序选择
+    schedule_types = {"Sequential": BaseScheduler,
+                      "Random": RandomActivation,
+                      "Simultaneous": SimultaneousActivation}
+
+    def __init__(self, num_agents=100, intense=0.1, schedule_type="Random", b=2, c=2, k=1.5, f=1, e=5, noise=0.7, mu=0.00015, t=1, seed=None):    # 删除ratio
+       # 博弈顺序确定
+        super().__init__()
+        #
+        self.num_agents = num_agents        
+        self.intense = intense
+
+        # 记录第几次运行模型
+        self.uid = next(self.id_gen) 
+        ## 
+        self.noise = noise                                              
+        self.mu = mu
+        self.f = f
+        self.e = e
+        self.t = t
+
+        self.k = k 
+        self.c = c
+        self.b = b
+#        self.pd_payoff = ((b-c,        -c,      b-c),
+#                          (  b,         0,      b-e),
+#                          (b-c,      -c-f,      b-c))                                      
+        self.pd_payoff = ((b-c,        -c,      -c-e),
+                          (  b,         0,      -e),
+                          (b-f,        -f,      -f-e))                                      
+        ##
+        self.schedule_type = schedule_type
+        self.schedule = self.schedule_types[self.schedule_type](self)
+        ##
+        
+       # 初始化agent
+        for i in range(self.num_agents):
+            agent = PDAgent(i, self)
+            self.schedule.add(agent)
+       # 初始化需要手机的数据和内容
+        self.datacollector = DataCollector(model_reporters={
+                                           "Cooperation": cooperate,
+                                           "Defect": defect,
+                                           "Punisher": punish,
+                                           })
+
+        self.running = True
+
+    def step(self):
+        self.schedule.step()
+        self.datacollector.collect(self)
+
+
+#def run(input):
+def run():
+#    a,b=input
+    br_params = {
+        'noise':[0.1],
+        'intense':[1],
+#        'b':[a],
+#        'c':[b],
+        'b':[16],
+        'c':[15],
+        'k':[6],
+        'f':[5],
+        'e':[5],
+#        'mu':[i/10000 for i in range(0,51,5)]
+        'mu':[0.0001]
+    }
+
+    br = batch_run(PdNetworkModel,
+                       number_processes=cpu_count(),  
+                       parameters=br_params,
+                       iterations=1,
+                       max_steps=10000,
+                       data_collection_period=1000
+#                       model_reporters={"Data Collector": lambda m: m.datacollector}
+                       )
+#    print(br)
+    result = pd.DataFrame(br)
+    
+#    result.index.names=['noise','intense','b','c','k','f','e','mu','iter','step']
+#    result.index.names=['b','k','f','e','iter','step']
+#    result.to_csv("../"+str(a)+"+"+str(b)+".csv")
+    result.to_csv("../new.csv")
+
+
+if __name__ == '__main__':
+#    input=[(i+1,i) for i in range(6,16,1)]
+#    for i in input:
+#        run(i)
+    start=time.time()
+    run()
+    end=time.time()
+    print(end-start)
+    exit()
---
+++ pd_punish/pd_punish/agent.py
@@ -0,0 +1,61 @@
+from mesa import Agent
+import random
+import numpy as np
+
+    
+def fitness(intense, payoff, action, noise, k, f, c, t):
+    '''
+    带惩罚机制的适应度计算  20210512
+    '''
+    if action == 0:   # 合作的适应度计算，只有合作的信号和合作的行为收益
+        f=0
+        k=0
+    elif action == 1: # 背叛的适应度计算，只有背叛信号和背叛行为的收益
+        c=0
+        f=0
+        k=0
+    else:   # 惩罚的适应度计算，只有惩罚行为和惩罚信号的收益
+        c=0
+                    # 善意惩罚的适应度计算
+    return np.exp(intense*payoff*(1-np.exp(-(noise+((1-noise)*t*(k*f+c))))))
+#    return np.exp(intense*payoff*(1-np.exp(-(1-noise)*(k*f+c))))
+
+
+
+
+class PDAgent(Agent):
+    def __init__(self, unique,  model, starting_move=None) -> None:
+        super().__init__(unique, model) 
+        self.score = 0
+        if starting_move:
+            self.move = starting_move
+        else:  
+            self.move = random.choice([0,1,2])                                         
+        self.next_move = random.choice([0,1,2])
+
+
+    def step(self):
+        if self.model.schedule_type != "Simultaneous":
+            self.advance()
+        if random.random() < self.model.mu:
+            self.next_move = random.choice([0,1,2])
+
+    
+    def advance(self):
+        self.move = self.next_move
+        self.score += self.increment_score()
+#        self.score = self.increment_score()
+
+    def increment_score(self):
+        self.other_agent = random.choice(self.model.schedule.agents)
+        if self.model.schedule_type == "Simultaneous":
+            move = self.other_agent.next_move 
+        else:
+            move = self.other_agent.move 
+        if self.other_agent.score > self.score:
+            self.next_move = self.other_agent.move
+        return fitness(self.model.intense, self.model.pd_payoff[self.move][move], self.move, self.model.noise, self.model.k, self.model.f, self.model.c, self.model.t) 
+            
+
+
+ 
